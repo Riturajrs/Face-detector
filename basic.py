d5f049b9c2a72abbb45af415b68e7840fb039b76
Rituraj Seal <rituraj2001.rs@gmail.com>
2021-08-24 13:41:34 +0530

    Labels added

--- basic.py
+++ basic.py
@@ -6,15 +6,16 @@ vid = cv2.VideoCapture(0)
 
 while True:
     check, frame = vid.read()
-    # frame = cv2.resize(frame, (int(frame.shape[1]/2),int(frame.shape[0]/2)))
     gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("Camera",gray_img)
     eye = eye_detect.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
     face = face_detect.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
     for x,y,w,h in face:
         frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+        cv2.putText(frame,'face',(int(x+w/2),y+h+25),cv2.FONT_ITALIC,1,(255,0,0),1,cv2.LINE_AA)
     for x,y,w,h in eye:
         frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        cv2.putText(frame,'eye',(int(x+w/2),y+h+25),cv2.FONT_ITALIC,1,(0,255,0),1,cv2.LINE_AA)
+    cv2.putText(frame,'Press \'q\' to quit',(25,25),cv2.FONT_ITALIC,0.5,(255,0,0),1,cv2.LINE_AA)
     cv2.imshow("Camera",frame)
     key = cv2.waitKey(1)
     if( key == ord('q') ):
